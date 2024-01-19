a7e9a13715398c35ee000ac71e6f736a9aba6c3b
Valeryia Tselesh <valeryiatselesh@gmail.com>
2024-01-19 16:10:14 +0100

    feat: add 01-read-file solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path'); 
+
+const stream = fs.createReadStream(path.resolve(__dirname, 'text.txt'));
+
+stream.on('data', function (chunk) {
+   console.log(chunk.toString());
+});
+
+
